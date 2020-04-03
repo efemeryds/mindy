8731b4aa754f2f7e2085a4ae4a493b7305ff35d8
Tomasz Dłuski <mr.toumash@gmail.com>
2020-04-03 23:34:18 +0200

    adds task back button

--- src/pages/SingleTaskPage.tsx
+++ src/pages/SingleTaskPage.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg } from '@ionic/react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+} from "@ionic/react";
+import { useParams } from "react-router-dom";
 
 const SingleTaskPage: React.FC = () => {
-    let {taskId} = useParams();
-    console.log('userId',taskId);
+  let { taskId } = useParams();
+  
+  console.log("userId", taskId);
   return (
     <IonPage>
+      <IonContent>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Mood</IonTitle>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/task"/>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Mood Page</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-    <h1>Task:{taskId}</h1>
+        <h1>Task:{taskId}</h1>
       </IonContent>
     </IonPage>
   );
