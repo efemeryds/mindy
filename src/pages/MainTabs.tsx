93b92e52ba5f9f1179c8bb4ef1b1a03ad910b9cb
Tomasz Dłuski <mr.toumash@gmail.com>
2020-04-04 18:18:03 +0200

    working taskDetailPage

--- src/pages/MainTabs.tsx
+++ src/pages/MainTabs.tsx
@@ -12,6 +12,7 @@ import Mood from './Mood'
 import Mind from './Mind';
 import Talks from './Talks';
 import Home from "./Home";
+import TaskDetailPage from './TaskDetailPage';
 
 interface MainTabsProps {}
 
@@ -36,8 +37,7 @@ const MainTabs: React.FC<MainTabsProps> = () => {
           component={SpeakerDetail}
           exact={true}
         />
-        <Route path="/tabs/task/:id" component={TaskDetail} />
-        <Route path="/tabs/speakers/sessions/:id" component={TaskDetail} />
+        <Route path="/tabs/task/:id" component={TaskDetailPage} />
         <Route path="/tabs/about" render={() => <About />} exact={true} />
         <Route path="/tabs/mood" render={() => <Mood />} exact={true} />
 
