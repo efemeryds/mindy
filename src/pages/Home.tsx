f888171ac4f2e9786b04762b409069ed70885791
Serp4 <bartosz.kusiak@nexpertis.pl>
2020-04-04 15:39:15 +0200

    Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import {
+  IonHeader,
+  IonToolbar,
+  IonContent,
+  IonPage,
+  IonButtons,
+  IonMenuButton,
+  IonLabel,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonAvatar,
+} from "@ionic/react";
+import "./Home.scss";
+import { useHistory } from "react-router";
+
+interface IconNavProps {
+  text: string;
+  iconName: string;
+  url: string;
+}
+
+const IconNav = (props: IconNavProps) => {
+  let history = useHistory();
+  const handle = () => {
+    history.push(props.url);
+  };
+  return (
+    <>
+      <div className="center" onClick={handle}>
+        <IonAvatar class="avatr">
+          <img src={"assets/icon/" + props.iconName} />
+        </IonAvatar>
+        <IonLabel>
+          <h2>{props.text}</h2>
+        </IonLabel>
+      </div>
+    </>
+  );
+};
+
+const Home: React.FC = () => {
+  return (
+    <IonPage id="home-page">
+      <IonContent>
+        <IonHeader className="ion-no-border">
+          <IonToolbar></IonToolbar>
+        </IonHeader>
+
+        <div className="home-title">
+          Hello <p>my friend</p>
+        </div>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IconNav url="/tasks" text="Tasks" iconName="tasks.png" />
+            </IonCol>
+            <IonCol>
+              <IconNav url="/topic" text="Topic" iconName="topics.png" />
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IconNav url="/mood" text="Mood" iconName="mood.png" />
+            </IonCol>
+            <IonCol>
+              <IconNav url="/mind" text="Mind" iconName="mind.png" />
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IconNav url="/cloud" text="Cloud" iconName="cloud.png" />
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default React.memo(Home);
--- src/pages/MainTabs.tsx
+++ src/pages/MainTabs.tsx
@@ -1,35 +1,57 @@
-import React  from 'react';
-import { IonTabs, IonRouterOutlet, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react';
-import { Route, Redirect } from 'react-router';
-import { calendar, location, informationCircle, people } from 'ionicons/icons';
-import SchedulePage from './SchedulePage';
-import SpeakerList from './SpeakerList';
-import SpeakerDetail from './SpeakerDetail';
-import SessionDetail from './SessionDetail';
-import MapView from './MapView';
-import About from './About';
+import React from "react";
+import {
+  IonTabs,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonIcon,
+  IonLabel,
+} from "@ionic/react";
+import { Route, Redirect } from "react-router";
+import { calendar, homeSharp, informationCircle, people } from "ionicons/icons";
+import SchedulePage from "./SchedulePage";
+import SpeakerList from "./SpeakerList";
+import SpeakerDetail from "./SpeakerDetail";
+import SessionDetail from "./SessionDetail";
+import About from "./About";
+import Home from "./Home";
 
-interface MainTabsProps { }
+interface MainTabsProps {}
 
 const MainTabs: React.FC<MainTabsProps> = () => {
-
   return (
     <IonTabs>
       <IonRouterOutlet>
-        <Redirect exact path="/tabs" to="/tabs/schedule" />
+        <Redirect exact path="/tabs" to="/tabs/home" />
         {/*
           Using the render method prop cuts down the number of renders your components will have due to route changes.
           Use the component prop when your component depends on the RouterComponentProps passed in automatically.
         */}
-        <Route path="/tabs/schedule" render={() => <SchedulePage />} exact={true} />
-        <Route path="/tabs/speakers" render={() => <SpeakerList />} exact={true} />
-        <Route path="/tabs/speakers/:id" component={SpeakerDetail} exact={true} />
+        <Route path="/tabs/home" render={() => <Home />} exact={true} />
+        <Route
+          path="/tabs/schedule"
+          render={() => <SchedulePage />}
+          exact={true}
+        />
+        <Route
+          path="/tabs/speakers"
+          render={() => <SpeakerList />}
+          exact={true}
+        />
+        <Route
+          path="/tabs/speakers/:id"
+          component={SpeakerDetail}
+          exact={true}
+        />
         <Route path="/tabs/schedule/:id" component={SessionDetail} />
         <Route path="/tabs/speakers/sessions/:id" component={SessionDetail} />
-        <Route path="/tabs/map" render={() => <MapView />} exact={true} />
         <Route path="/tabs/about" render={() => <About />} exact={true} />
       </IonRouterOutlet>
       <IonTabBar slot="bottom">
+        <IonTabButton tab="home" href="/tabs/home">
+          <IonIcon icon={homeSharp} />
+          <IonLabel>Home</IonLabel>
+        </IonTabButton>
         <IonTabButton tab="schedule" href="/tabs/schedule">
           <IonIcon icon={calendar} />
           <IonLabel>Schedule</IonLabel>
@@ -37,10 +59,6 @@ const MainTabs: React.FC<MainTabsProps> = () => {
         <IonTabButton tab="speakers" href="/tabs/speakers">
           <IonIcon icon={people} />
           <IonLabel>Speakers</IonLabel>
-        </IonTabButton>
-        <IonTabButton tab="map" href="/tabs/map">
-          <IonIcon icon={location} />
-          <IonLabel>Map</IonLabel>
         </IonTabButton>
         <IonTabButton tab="about" href="/tabs/about">
           <IonIcon icon={informationCircle} />
